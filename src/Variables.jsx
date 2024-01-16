bac8f00c66921e5d88b715d156d03d8dbeecc9d7
Believeharsh <bontinue@gmail.com>
2024-01-16 12:29:00 +0530

    Understood about active and focus Utliltiy in Tailwind CSS

--- src/Variables.jsx
+++ src/Variables.jsx
@@ -4,12 +4,22 @@
 function Variables() {
   return (
    <>
-    <div className="LearnApply">
-     <div>Harsh Dahiya</div>
-     <div></div>
-     <div></div>
-     <div></div>
-     <div></div>
+    <div className="bg-black w-full h-[100vh]">
+      <div className="flex justify-center items-center pt-10 space-x-2">
+      <div className="">
+        <button className="bg-blue-500 rounded-xl text-white font-mono px-2 py-1 active:bg-blue-900  focus:bg-slate-900 focus:border-white border-[1px]  ">YourBTn</button>
+      </div>
+      <div className="bg-white rounded-xl text-black font-mono px-2 py-1  focus:bg-gray-700 focus:border-white focus:border-[1px]  ">
+        <button>HarshBTn</button>
+      </div>
+      <div className="bg-purple-500 rounded-xl text-white font-mono px-2 py-1 active:bg-purple-900 active:border-white active:border-[0.5px] ">
+        <button>AsyabBTn</button>
+      </div>
+      <div className="bg-orange-500 rounded-xl text-white font-mono px-2 py-1 active:bg-orange-900 focus:bg-slate-900 focus:border-white focus:border-[1px] ">
+        <button>ShardaBTn</button>
+      </div>
+
+      </div>
     </div>
    </>
   )
